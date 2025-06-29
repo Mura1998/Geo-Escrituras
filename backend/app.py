c4009e4bfcadd0c6d13d7b12b934f6a43730b2ac
Mura1998 <ramosjuan258@gmail.com>
2025-06-28 21:16:08 -0600

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -56,4 +56,6 @@ def procesar_escritura():
     return jsonify({"texto_extraido": texto, "datos_tecnicos": datos})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
