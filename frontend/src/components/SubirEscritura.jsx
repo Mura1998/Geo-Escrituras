e58f4752dc474d61b7f07478ea3a570cd5700be0
Mura1998 <ramosjuan258@gmail.com>
2025-08-02 22:04:41 -0600

    Update SubirEscritura.jsx

--- frontend/src/components/SubirEscritura.jsx
+++ frontend/src/components/SubirEscritura.jsx
@@ -1,3 +1,4 @@
+console.log('BACKEND_URL:', import.meta.env.VITE_BACKEND_URL);
 import { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
